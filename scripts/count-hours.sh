7f92f8168aa00336b30b18a9f2e17165d485cfee
Frederik Kutsch <frederik.kutsch@yahoo.com>
2023-12-10 09:55:48 +0100

    Improve path comments

--- scripts/count-hours.sh
+++ scripts/count-hours.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export to CSV
-osascript /Users/my.user/path/to/export-numbers-to-csv.sh /Users/my.user/path/to/folder/containing/hours.numbers # Adapt the two paths. NOTE: The second path leads to the folder that contains the 'hour.numbers' file, NOT the file itself.
+osascript /Users/my.user/path/to/export-numbers-to-csv.sh /Users/my.user/path/to/hours.numbers/folder # Adapt the two paths. NOTE: The second path leads to the folder that contains the 'hour.numbers' file, NOT the file itself.
 
 # Run script to output table
 cd /Users/my.user/path/to/repository/count-hours # Adapt the path.
